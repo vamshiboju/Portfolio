9210246c9118f86a7d056948958fbfc1b18a2a74
Vamshi Krishna Boju <83817973+vamshiboju@users.noreply.github.com>
2024-12-30 17:38:53 -0500

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/Vamshi_Krishna_Resume_F.pdf";
+import pdf from "../../Assets/Vamshi_Krishna_Boju_Resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
